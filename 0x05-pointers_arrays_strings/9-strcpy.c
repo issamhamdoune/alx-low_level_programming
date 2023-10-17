16781dd948773d925a804d4cf9445477d3239bc1
issamhamdoune <issamhamdoune1@gmail.com>
2023-10-17 10:35:44 -0700

    a

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int 1 = 0;
-	int x = 0;
+	int 1 = -1;
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
 
-	while (*(src + 1) != '\0')
-	{
-		1++;
-	}
-	for ( ; x < 1 ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[1] = '\0';
 	return (dest);
 }
