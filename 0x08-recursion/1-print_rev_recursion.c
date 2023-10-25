a1ca10bbc6a7e3a7bbc7ad72803acfee55c35928
issamhamdoune <issamhamdoune1@gmail.com>
2023-10-25 09:20:16 -0700

    a

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints string in reverse
+ * _print_rev_recursion - Prints a string in reverse
  * @s: The string to be printed.
  */
 
@@ -10,6 +10,6 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*S);
+		_putchar(*s);
 	}
 }
