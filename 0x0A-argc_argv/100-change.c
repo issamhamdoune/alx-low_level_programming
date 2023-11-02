0cd788a10779d444c80d02fb9365400ae9bb0844
issamhamdoune <issamhamdoune1@gmail.com>
2023-11-01 17:06:44 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,38 +3,38 @@
 
 /**
  * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
+ * make change for an amount of money.
+ * @argc: number of commandline arguments.
+ * @argv: pointer to an array of command line arguments.
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0-success, non-zero-fall.
  */
 
 int main(int argc, char *argv[])
 {
-int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
+	if (argc == 2)
+	{
+		int i, leastcents = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				leastcents += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("d\n", leastcents);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	result = 0;
-	if (num < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-		printf("%d\n", result);
-		return (0);
-	}
+	return (0);
 }
