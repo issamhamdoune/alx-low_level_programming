579724ed5c5bd48832d24e3d37717950e0f55ab9
issamhamdoune <issamhamdoune1@gmail.com>
2023-11-09 17:56:27 -0800

    a

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,29 +1,32 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - returns pointer to right op func
- * @s: string operator
+ * get_op_func - ...
+ * @s: ...
  *
- * Return: the right function pointer
+ * Return: ...
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
 	}
 
-	return (NULL);
+	return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,29 +1,32 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - check the code for the school students.
- * @argc: the number of args
- * @argv: argument vector
+ * main - ...
+ * @argc: ...
+ * @argv: ...
  *
- * Return: Always 0.
+ * Return: ...
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
-	int (*op_func)(int, int), a, b;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	oprt = get_op_func(argv[2]);
 
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("Error\n"), exit(99);
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(100);
-
-	printf("%d\n", op_func(a, b));
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
